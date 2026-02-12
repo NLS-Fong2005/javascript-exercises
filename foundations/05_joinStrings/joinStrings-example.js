d3b946a15470b4ca74b1d6f3de6903b2211c120e
NLS-Fong2005-Local <nixonlishen.fong2005@protonmail.com>
2026-02-12 15:30:52 +0800

    feat: solve foundations-05

--- foundations/05_joinStrings/joinStrings-example.js
+++ foundations/05_joinStrings/joinStrings-example.js
@@ -4,6 +4,7 @@ const firstName = "Carlos";
 const lastName = "Stevenson";
 
 const greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + (thisYear - birthYear) + " years old.";
+// "Hello! My name is Carlos Stevenson and I am 18 years old."
 
 console.log(greeting);
 
